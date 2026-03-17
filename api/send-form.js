c58a7cd7f04d199e98bb3646430ea4873413792c
jalejo-cyber <jalejo@fomentformacio.com>
2026-03-17 13:43:33 +0100

    Update send-form.js

--- api/send-form.js
+++ api/send-form.js
@@ -343,8 +343,8 @@ export default async function handler(req, res) {
     if (sigB64) {
       const sigImgAcords = await acordsDoc.embedPng(sigB64);
       acordsPage.drawImage(sigImgAcords, {
-        x: 283,
-        y: 57,
+        x: 180,
+        y: 80,
         width: 160,
         height: 50
       });
