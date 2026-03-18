9b6f27af6ea4bec0cece83d68741ce08e161ef8a
jalejo-cyber <jalejo@fomentformacio.com>
2026-03-18 10:47:49 +0100

    Update send-form.js

--- api/send-form.js
+++ api/send-form.js
@@ -435,7 +435,7 @@ setInforme(["nif"], getVal("dni"));
 // ❗ TELÈFON → IMPORTANT (2 camps iguals)
 // 0 = entitat
 // 1 = persona orientada
-setInformeIndex(["telefon"], 1, getVal("telefon"));
+setInformeIndex(["telefon_persona"], getVal("telefon"));
 
 // ✅ EMAIL (normalment no duplicat)
 setInforme(["correu"], getVal("email"));
